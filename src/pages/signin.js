96497e24cab80ba9ee76f845594da57a2a8a1397
nicolanzi1 <nicolanzi@gmail.com>
2020-10-14 00:21:15 +0100

    Wrote the Sign Up page, added firebase auth to both Sign In and Sign Up, and wrote helper routes methods to redirect users and protect routes within app.js

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
 import HeaderContainer from '../containers/header';
 import FooterContainer from '../containers/footer';
 import { Form } from '../components';
+import * as ROUTES from '../constants/routes';
 
 export default function SignIn() {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
@@ -13,7 +18,17 @@ export default function SignIn() {
   const handleSignin = (event) => {
     event.preventDefault();
 
-    // firebase work here!
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(emailAddress, password)
+      .then(() => {
+        history.push(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmailAddress('');
+        setPassword('');
+        setError(error.message);
+      });
   };
 
   return (
