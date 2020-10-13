51f2a42b06697018e3752b2d844f840a3b64dff6
nicolanzi1 <nicolanzi@gmail.com>
2020-10-13 18:28:47 +0100

    Connected the app to Firebase to be able to seed, generate and auth the db

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,18 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyDD8gw89Rsexxsqb_b8nlu5Uo66BxfyQsQ',
+  authDomain: 'nickflicks-ee6a4.firebaseapp.com',
+  databaseURL: 'https://nickflicks-ee6a4.firebaseio.com',
+  projectId: 'nickflicks-ee6a4',
+  storageBucket: 'nickflicks-ee6a4.appspot.com',
+  messagingSenderId: '557360279581',
+  appId: '1:557360279581:web:f672bbac8538be59c196f4',
+  measurementId: 'G-ET6JMCLR0Z',
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
