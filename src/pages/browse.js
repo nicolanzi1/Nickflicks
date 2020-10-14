7cb5d3fcc036674db460a4588df7b7adf98cb557
nicolanzi1 <nicolanzi@gmail.com>
2020-10-14 12:37:15 +0100

    Wrote the Profiles component and container, styled it and added it to the Browse page, whilst checkign it all renders correctly

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowseContainer } from '../containers/browse';
 import { useContent } from '../hooks';
 import selectionFilter from '../utils/selection-filter';
 
@@ -7,5 +8,5 @@ export default function Browse() {
   const { films } = useContent('films');
   const slides = selectionFilter({ series, films });
 
-  return <p>Hello from the browse!</p>;
+  return <BrowseContainer slides={slides} />;
 }
