422802cc9aaa7d40bea2cf3ee467a7e91ad7dae1
nicolanzi1 <nicolanzi@gmail.com>
2020-10-14 22:30:31 +0100

    Debugged the logo link back to the Home page, anc checked is working correctly

--- src/containers/header.js
+++ src/containers/header.js
@@ -7,7 +7,7 @@ export default function HeaderContainer({ children }) {
   return (
     <Header>
       <Header.Frame>
-        <Header.Logo to={ROUTES} src={logo} alt="Nickflicks" />
+        <Header.Logo to={ROUTES.HOME} src={logo} alt="Nickflicks" />
         <Header.ButtonLink to={ROUTES.SIGN_IN}>Sign In</Header.ButtonLink>
       </Header.Frame>
       {children}
