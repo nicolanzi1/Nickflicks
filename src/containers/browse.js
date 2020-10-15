0c285911da0289e431d9acddf9823f2a6e0b70f8
nicolanzi1 <nicolanzi@gmail.com>
2020-10-15 09:38:26 +0100

    Wrote the Live Search bar through useEffect and checked it works well to finish the development side, and now into testing for later deployment

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import React, { useContext, useState, useEffect } from 'react';
+import Fuse from 'fuse.js';
 import { SelectProfileContainer } from './profiles';
 import FooterContainer from './footer';
 import { FirebaseContext } from '../context/firebase';
@@ -25,6 +26,16 @@ export function BrowseContainer({ slides }) {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, { keys: ['data.description', 'data.title', 'data.genre'] });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
