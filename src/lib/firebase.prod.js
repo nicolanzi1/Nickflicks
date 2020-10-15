754d84868e0ff3f9fa60ebadbfec5c6f9e66f44c
Nicola Lanzillotta <62956447+nicolanzi1@users.noreply.github.com>
2020-10-15 23:04:36 +0100

    Update firebase.prod.js

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,16 +3,18 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyDD8gw89Rsexxsqb_b8nlu5Uo66BxfyQsQ',
-  authDomain: 'nickflicks-ee6a4.firebaseapp.com',
-  databaseURL: 'https://nickflicks-ee6a4.firebaseio.com',
-  projectId: 'nickflicks-ee6a4',
-  storageBucket: 'nickflicks-ee6a4.appspot.com',
-  messagingSenderId: '557360279581',
-  appId: '1:557360279581:web:f672bbac8538be59c196f4',
-  measurementId: 'G-ET6JMCLR0Z',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 const firebase = Firebase.initializeApp(config);
+
+// seedDatabase(firebase);
 
 export { firebase };
